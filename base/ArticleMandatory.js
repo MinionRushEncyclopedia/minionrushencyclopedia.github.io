cb7552f17c399b2c2177fe73dba43db5d65fd136
MinionRushEncyclopedia <85449359+MinionRushEncyclopedia@users.noreply.github.com>
2021-09-09 20:36:41 -0400

    JS Improvements

--- base/ArticleMandatory.js
+++ base/ArticleMandatory.js
@@ -1,6 +1,7 @@
 var page = document.getElementsByClassName("page")[0];
 
 var article = document.getElementById('Article').innerHTML;
+var article_extended = document.getElementById('article_extended').innerHTML;
 var article_nav = document.getElementById('article_nav').innerHTML;
 var media = document.getElementById('Media').innerHTML;
 var charts = document.getElementById('Charts').innerHTML;
@@ -11,8 +12,10 @@ var links = document.getElementById('Links').innerHTML;
 page.innerHTML = "<div class='loading'> </div>";
 
 
+var DiscordWidget = '<iframe id="discordWidget" class="long_iframe" src="https://discord.com/widget?id=755050417485906000&theme=dark" style="margin-bottom: 25px;" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts">h</iframe>';
+
 media = media.replace('BigMediaBox', 'BigMediaBox');
-page.innerHTML = article + article_nav + charts + media + triviaLinks;
+page.innerHTML = article + article_nav + article_extended + DiscordWidget + charts + media + triviaLinks;
 console.log(media);
 
 
@@ -25,3 +28,19 @@ function removeImageDescription() {
 	document.getElementById('Media').getElementsByClassName('paragraph')[0].innerHTML = "";
 	document.getElementById('Media').getElementsByClassName('paragraph')[0].style.paddingBottom = "45px";
 }
+
+var mediaQuery = window.matchMedia('(max-width: 720px)')
+			
+function handleTabletChange(e) {
+	if (e.matches) {
+		document.getElementById("discordWidget").style["display"] = "none";
+		document.getElementById('article_extended').style.width = "90%";
+	  }
+    else {
+		document.getElementById("discordWidget").style["display"] = "inline-block";
+		document.getElementById('article_extended').style.width = "60%";
+	  }
+	}
+				
+	mediaQuery.addListener(handleTabletChange)
+	handleTabletChange(mediaQuery)
